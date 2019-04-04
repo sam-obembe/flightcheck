9e28ef5ae94aed7cf122f3df85d24bfdab382e35
sam-obembe <seyanuobembe@gmail.com>
2019-04-04 16:04:10 -0500

    changed landing Nav

--- src/components/Landing/LandingNav.js
+++ src/components/Landing/LandingNav.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import AppBar from '@material-ui/core/AppBar'
-import Typography from '@material-ui/core/Typography'
+
 
 const LandingNav = ()=>{
   return(
     <div style = {{marginBottom: "100px"}}>
       <AppBar>
-        <Typography variant = "h4">Flight Search</Typography>
+      <img src = {require("../../paper-plane.png")} alt = "logo" width = "50px"/>
       </AppBar>
     </div>
   
